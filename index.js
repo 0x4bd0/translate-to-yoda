2cd6fee01ed36e96b3d31450834ebe91abbea9de
abdelkader MOUDJAR <abdoumjr@gmail.com>
2021-05-20 11:21:25 +0100

    :fire:

---
+++ index.js
@@ -0,0 +1,18 @@
+const data = process.argv.slice(2);
+
+const length = data.length
+const half = length/2
+
+let res  = []
+
+if( length%2 == 0 ){
+    res.push(...data.slice(half,data.length))
+    res.push(',')
+    res.push(...data.slice(0,half))
+}else {
+    res.push(...data.slice(half+1,data.length))
+    res.push(',')
+    res.push(...data.slice(0,half+1))
+}
+
+console.log(res.join(' '))
